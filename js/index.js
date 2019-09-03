846f36e1b2eb6b9a9d8a252aea89337bd62bb44e
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-03 22:37:00 +0100

    added the keydown

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 // Your code goes here
 let myHome = document.querySelector ('.nav-link');
-myHome.addEventListener('mouseover', (event) => {event.target.style.color = 'purple'; });
+myHome.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'purple'; });
+
+// 2
+let newEffect = document.querySelector('.main-navigation')
+newEffect.addEventListener('keydown', (event) => {
+event.target.style.background = 'red';
+});
+
+//3
+
