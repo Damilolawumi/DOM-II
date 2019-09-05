a89d1df1e98ab916ae5a03f6d5a691342387c070
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-05 12:35:54 +0100

    added the resize event, online event and load event

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
 // Your code goes here
+
+// mouseover
 let myHome = document.querySelector('.nav-link');
 myHome.addEventListener('mouseover', (event) => {
-    event.stopPropagation();
+    event.stopPropagation(); //stop propagation
     event.target.style.color = 'purple';
 });
 
-// 2
+// 2 keydown
 let anInput = document.querySelector('input')
 anInput.addEventListener('keydown', (event) => {
     anInput.style.background = 'aqua';
 });
 
-//3
+//3 dblclick
 let doubleClickEffect = document.querySelector('h2')
 doubleClickEffect.addEventListener('dblclick', () => { doubleClickEffect.classList.toggle('large') })
 
-//4
+//4 select
 function logSelection(event) {
     const log = document.getElementById('log');
     const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
@@ -25,20 +27,20 @@ function logSelection(event) {
 const input = document.querySelector('input');
 input.addEventListener('select', logSelection);
 
-//5
+//5 scroll
 
 window.addEventListener('scroll', (event) => {
-    // alert('I have been scrolled')
+    // alert('I have been scrolled').. commemnted it out cause the alert kept popping
 })
 
 
-//6
+//6 focus
 input.addEventListener('focus', (event) => {
     input.style.backgroundColor = 'green';
     input.style.color = 'white'
 })
 
-//7
+//7 blur
 input.addEventListener('blur', (event) => {
     input.style.backgroundColor = 'white';
     input.style.color = 'red'
@@ -53,4 +55,19 @@ nav.addEventListener('mouseover', (event) => {
 //add click to home nav
 myHome.addEventListener('click', (event) => {
     event.preventDefault()
+});
+
+//8 Load
+window.addEventListener('load', (event) => {
+    alert('page is fully loaded');
+  });
+
+  //9 online
+ window.addEventListener('online', (event) => {
+    alert("You are now connected to the network.");
+});
+
+//10 resize
+window.addEventListener('resize', (event) => { 
+    alert ('your screen got resized');
 });
